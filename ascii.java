db6302e23c68dd1b240ef51f8d94f980abf9b97f
chintupranay <131235262+chintupranay@users.noreply.github.com>
2023-06-02 17:53:55 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ class Program207
+{
+public static void main(String args[])
+{
+char ch;
+Scanner scob=new Scanner(System.in);
+ch=scob.next().charAt(0);
+int x=(int)ch;
+
+System.out.println("ASCII value of"+ch+"is"+x);
+}
+} 
